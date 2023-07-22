7bb50ab1b9972495c9a42780bfc45c56ddd4535a
Beckmen1 <140009453+Beckmen1@users.noreply.github.com>
2023-07-22 14:24:53 +0400

    Add files via upload

---
+++ dey 5/dey5.py
@@ -0,0 +1,22 @@
+name = input("რა გქვია: ")
+print("მისი სახელია: " + name)
+
+username = input("რა არის შენი გვარი: ")
+print("მისი სახელია: " + name +" " "მისი" +"გვარია"" " + username)
+
+food = input("რა არის შენი საყვარელი საჭმელი: ")
+print("შესანიშნავი არჩევანია"" " + food)
+
+country = input("რომელ ქვეყანაში ცხოვრობ: ")
+print("იდეალური ადგილია"" " + country)
+
+the_best_place = input("რა არის შენი საყვარელი ადგილი: ")
+print("wow")
+
+profession = input("რა არის შენი პროფესია: ")
+print("good choise meen ")
+
+future = input("სად ხედავ შენს თავს მომავალში: ")
+print("GOOD LUCK: ")
+
+
