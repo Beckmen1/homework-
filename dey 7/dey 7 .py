63864de30302b06ad8f06598e363f1829a9eb502
Beckmen1 <140009453+Beckmen1@users.noreply.github.com>
2023-07-29 18:21:54 +0400

    Add files via upload

---
+++ dey 7/dey 7 .py
@@ -0,0 +1,12 @@
+age = 15
+time = True
+print(age > 15 or time)
+
+goa_is_the_best = True
+nikas_age =28
+print(goa_is_the_best and nikas_age <30 )
+
+fruit = True
+ROBLOX = False
+print(fruit and ROBLOX)
+
