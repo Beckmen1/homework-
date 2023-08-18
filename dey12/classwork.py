d6980ce58d301f2c332214dba68f85ff080f03c4
Beckmen1 <140009453+Beckmen1@users.noreply.github.com>
2023-08-18 16:29:50 +0400

    Add files via upload

---
+++ dey12/classwork.py
@@ -0,0 +1,13 @@
+# # item1 = "milk"
+# # item2 = "tea"
+# # item3 ="banana"
+
+
+# # list=[ "milk","tea","banana"]
+# # print(list[2]+"20")
+
+# femilly = ["me","ganabi deda""debi"]
+# femilly[0] ="qatamadze"
+# print(femilly[2])
+
+#
---
+++ dey12/homeworkt.py
@@ -0,0 +1,14 @@
+# name =["n","i","k","a"]
+# surname = ["qatamadze"]
+# print("xmovnebis raodenobaa: " + str(7))
+
+name1 = "nika" 
+name2 = "tamuna"
+name3 = "olegi"
+
+surname = "qatamadze"
+surname2 ="sisvadze"
+surname3 = "qatamadze"
+
+list =["nika","tamuna","olegi","qatamadze","sisvadze","qatamadze"]
+print(list[0] + list[3],list[1] + list[4],list[2] + list[5])
