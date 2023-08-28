1d086b85d4814c8e02af861ec7cb0cdc40095cf4
Beckmen1 <140009453+Beckmen1@users.noreply.github.com>
2023-08-28 23:21:26 +0400

    Add files via upload

---
+++ dey15/classworl.py
@@ -0,0 +1,10 @@
+gela = ["12" , "13" , ("tuple",14)]
+print(gela[2:])
+
+
+
+
+name = "nika"
+name[2] = "t"
+
+
---
+++ dey15/homework.py
@@ -0,0 +1,24 @@
+country = "saqartvelo"
+print(country[2:])
+print(country[4:])
+print(country[2:9] + 'i')
+print(country[4:9] + 'i')
+print(country[2:6] + 'uli')
+
+books =['თამაში ჭვავის ყანაში', 'ზებუნებრივი' , 'შერლოკ ჰოუმსი' ,'3 მუშკეტერი(ახალი)' , 'The most favorite book of childhood(პიტერპენი)']
+print("leadeboard")
+
+print("1st")
+print(books[1:2])
+
+print('2st')
+print(books[3:4])
+
+print('3st')
+print(books[2:3])
+
+print('4st')
+print(books[0:1])
+
+print('5st')
+print(books[4:5])
