9f8411e304ad69956f83b326afd557e9668e4192
Beckmen1 <140009453+Beckmen1@users.noreply.github.com>
2023-09-06 17:37:56 +0400

    Add files via upload

---
+++ dey17/klasswork.py
@@ -0,0 +1,13 @@
+import random 
+crew_leaders = ['kruashvili', 'amiranashvili', 'tyeshelashvili', 'janezashvili', 'molodini', 'kereselidze', 'kurtanidze','aruda']
+
+# print(crew_leaders[4])
+# print(crew_leaders[random.randint(0,6)]) 
+# print(crew_leaders[random.randint(0,6)])
+for i in range(1,4):
+    print('winner N',i,'is',random.choice(crew_leaders))
+
+crew_leaders.remove('aruda')
+
+
+print(crew_leaders) 
